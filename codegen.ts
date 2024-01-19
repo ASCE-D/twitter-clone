1a339442efc7d707c931d93aa1de355be45a2132
Deepesh Genani <72039675+ASCE-D@users.noreply.github.com>
2024-01-19 23:57:35 +0530

    Update codegen.ts

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "http://65.1.67.38:8000/graphql",
+  schema: "https://d1nwp5aagmh196.cloudfront.net/graphql",
   documents: "**/*.{tsx,ts}",
   generates: {
     "gql/": {
